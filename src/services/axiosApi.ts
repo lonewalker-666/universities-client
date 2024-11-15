c4b8d8d04a70be9076dfb70eaa1bb271ec4db0af
= <=>
2024-11-15 23:34:10 +0530

    test

--- src/services/axiosApi.ts
+++ src/services/axiosApi.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 // eslint-disable-next-line import/prefer-default-export
 export const ApiCall = axios.create({
-  baseURL: "http://127.1.0.0:8000",
-
+  // baseURL: "http://127.1.0.0:8000",
+     baseURL: "https://dev.theuniversitiesusa.com"
 });
