56531cc805c59d2841e0d6c1b76e6046c9cd4779
= <=>
2024-11-15 23:00:33 +0530

    fixed

--- src/components/home/components/testimonial.tsx
+++ src/components/home/components/testimonial.tsx
@@ -9,7 +9,7 @@ const Testimonials = () => {
         <HeaderContainer
           title='Testimonials'
           heading='Voices of Experience'
-          description="Insights from students who've walked the path."
+          description="Insights from students who have walked the path."
 
         />
       </div>
