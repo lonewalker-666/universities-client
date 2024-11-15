54b92a5f0fc69748a7dc939686017c3ceca101d0
Karishma0428 <karishmakarishma288@gmail.com>
2024-11-15 21:27:46 +0530

    commint

--- src/components/onboarding/step1.tsx
+++ src/components/onboarding/step1.tsx
@@ -74,82 +74,78 @@ const Step1 = () => {
         </div>
       </div>
 
-      <div className="max-h-64 overflow-y-auto p-2 rounded-lg hide-scroll">
-        <div className="mb-4">
-          <label className="block text-sm font-medium">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={(e: any) => handleChange(e)}
-            className={`bg-[#FAFAFA] rounded-md text-lg w-full p-2 font-medium ${
-              errors.email ? "border-red-600" : "border-gray-300"
-            }`}
-            placeholder="Enter your Email"
-          />
-          {errors.email && (
-            <p className="text-red-600 text-sm">{errors.email}</p>
-          )}
-        </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={(e: any) => handleChange(e)}
+          className={`bg-[#FAFAFA] rounded-md text-lg w-full p-2 font-medium ${
+            errors.email ? "border-red-600" : "border-gray-300"
+          }`}
+          placeholder="Enter your Email"
+        />
+        {errors.email && <p className="text-red-600 text-sm">{errors.email}</p>}
+      </div>
 
-        <div className="mb-4">
-          <label className="block text-sm font-medium">Phone Number</label>
-          <input
-            type="tel"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={(e: any) => handleChange(e)}
-            className={`bg-[#FAFAFA] rounded-md text-lg w-full p-2 font-medium ${
-              errors.phoneNumber ? "border-red-600" : "border-gray-300"
-            }`}
-            placeholder="Enter your Phone Number"
-          />
-          {errors.phoneNumber && (
-            <p className="text-red-600 text-sm">{errors.phoneNumber}</p>
-          )}
-        </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium">Phone Number</label>
+        <input
+          type="tel"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={(e: any) => handleChange(e)}
+          className={`bg-[#FAFAFA] rounded-md text-lg w-full p-2 font-medium ${
+            errors.phoneNumber ? "border-red-600" : "border-gray-300"
+          }`}
+          placeholder="Enter your Phone Number"
+        />
+        {errors.phoneNumber && (
+          <p className="text-red-600 text-sm">{errors.phoneNumber}</p>
+        )}
+      </div>
 
-        <div className="mb-4">
-          <label className="block text-sm font-medium">Gender</label>
-          <select
-            name="gender"
-            value={formData.gender}
-            onChange={(e: any) => handleChange(e)}
-            className={` p-2 rounded-md bg-[#FAFAFA] font-medium w-full ${
-              errors.gender ? "border-red-600" : "border-gray-300"
-            }`}
-          >
-            {genderOptions.map((option, index) => (
-              <option key={index} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-          {errors.gender && (
-            <p className="text-red-600 text-sm">{errors.gender}</p>
-          )}
-        </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium">Gender</label>
+        <select
+          name="gender"
+          value={formData.gender}
+          onChange={(e: any) => handleChange(e)}
+          className={` p-2 rounded-md bg-[#FAFAFA] font-medium w-full ${
+            errors.gender ? "border-red-600" : "border-gray-300"
+          }`}
+        >
+          {genderOptions.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        {errors.gender && (
+          <p className="text-red-600 text-sm">{errors.gender}</p>
+        )}
+      </div>
 
-        <div className="mb-4">
-          <label className="block text-sm font-medium ">Nationality</label>
-          <select
-            name="nationality"
-            value={formData.nationality}
-            onChange={(e: any) => handleChange(e)}
-            className={` p-2 rounded-md bg-[#FAFAFA] font-medium w-full ${
-              errors.nationality ? "border-red-600" : "border-gray-300"
-            }`}
-          >
-            {nationalityOptions.map((option, index) => (
-              <option key={index} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-          {errors.nationality && (
-            <p className="text-red-600 text-sm">{errors.nationality}</p>
-          )}
-        </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium ">Nationality</label>
+        <select
+          name="nationality"
+          value={formData.nationality}
+          onChange={(e: any) => handleChange(e)}
+          className={` p-2 rounded-md bg-[#FAFAFA] font-medium w-full ${
+            errors.nationality ? "border-red-600" : "border-gray-300"
+          }`}
+        >
+          {nationalityOptions.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        {errors.nationality && (
+          <p className="text-red-600 text-sm">{errors.nationality}</p>
+        )}
       </div>
     </div>
   );
