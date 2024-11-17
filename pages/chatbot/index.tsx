de0c3a480214487718bf0207eac7099f922c2f73
= <=>
2024-11-17 16:15:23 +0530

    empty chat

---
+++ pages/chatbot/index.tsx
@@ -0,0 +1,13 @@
+import ChatbotContainer from "@/src/components/chatbot"
+import ProductLayout from "@/src/components/layout/productLayout"
+
+
+const CollegesPage = () => {
+  return (
+    <ProductLayout title="Universities@USA-Chatbot" >
+      <ChatbotContainer />
+    </ProductLayout>
+  )
+}
+
+export default CollegesPage
